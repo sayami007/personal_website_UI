e14f5a46291d2f77c5625cb26ed55fa56c2d12e6
Bibesh Manandhar <bmanandhar@advalent.com>
2023-04-01 00:14:18 +0545

    INIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable, ObservableLike, take } from 'rxjs';
+
+interface Users {
+  UserKey: number;
+  UserName: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'personal';
+export class AppComponent implements OnInit {
+  public userList$!: Observable<Users[]> ;
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.userList$ = this.httpClient.get(
+      'https://polydactyl-creative-ravioli.glitch.me/users'
+    ) as Observable<Users[]>;
+  }
 }
