cc93906a07e7ffb59bbbf73b0d4f6e752390db8f
Bibesh <bmanandhar@advalent.com>
2024-01-29 17:10:40 +0545

    Updated to Typescript with tailwind

---
+++ src/pages/LandingPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+
+function LandingPage() {
+  return (
+    <div className='flex bg-red-200'>
+      <Navbar></Navbar>
+      <div className='bg-red-400 decoration-dotted'>hellow</div>  
+    </div>
+  );
+}
+
+export default LandingPage;
